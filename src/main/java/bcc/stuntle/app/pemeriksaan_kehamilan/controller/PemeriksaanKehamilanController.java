125dfb980aa7eef363ad7ec750104b4458cd8175
VinncentWong <vinncentwong2@gmail.com>
2024-01-23 20:54:47 +0700

    [CHANGE] pre authorize

--- src/main/java/bcc/stuntle/app/pemeriksaan_kehamilan/controller/PemeriksaanKehamilanController.java
+++ src/main/java/bcc/stuntle/app/pemeriksaan_kehamilan/controller/PemeriksaanKehamilanController.java
@@ -25,7 +25,7 @@ import java.util.List;
 @SecurityRequirement(name = "bearerAuth")
 @RestController
 @RequestMapping("/pemeriksaan/kehamilan")
-@PreAuthorize("hasRole('FASKES')")
+@PreAuthorize("hasAnyRole('ORANGTUA', FASKES')")
 public class PemeriksaanKehamilanController {
 
     @Autowired
